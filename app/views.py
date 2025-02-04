8c4f3ef98d63d08bb80560493232cc06354297ee
aditi saudagar <aditisaudagar17@gmail.com>
2025-02-04 16:09:58 +0530

    dtl

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+def home(request):
+    stu = Student.objects.all()
+    print(stu)
+    return render(request, 'home.html',{'data':stu.values()})
