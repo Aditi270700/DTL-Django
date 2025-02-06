3a3508d39c1d5028f8e6de3e6238ebf0f8191fb0
aditi saudagar <aditisaudagar17@gmail.com>
2025-02-06 16:04:53 +0530

    edit

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from app import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('delete/<int:pk>',views.delete,name='delete')
-    
+    path('delete/<int:pk>',views.delete,name='delete'),
+    path('edit/<int:pk>',views.edit,name='edit'),
+    path('updatedat/<int:pk>',views.updatedat,name='updatedat')
 ]
