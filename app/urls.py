acbaa1f5aae631d580fd38a6fff035d319a1f220
aditi saudagar <aditisaudagar17@gmail.com>
2025-02-06 14:46:45 +0530

    delete

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from app import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('delete/<int:pk>',views.delete,name='delete')
     
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,10 @@ def home(request):
     stu = Student.objects.all()
     print(stu)
     return render(request, 'home.html',{'data':stu.values(),'data2':"kuch nhi"})
+
+
+def delete(request,pk):
+    data = Student.objects.get(id=pk)
+    data.delete()
+    stu = Student.objects.all()
+    return render(request, 'home.html',{'data':stu})
